667a254d2381ae60b4c6903ca6db03fb994de2b4
김태형 <taehyeong224@gmail.com>
2019-06-01 15:51:49 +0900

    #1 https, http 를 선택하여 url 생성 기본값은 https 사용

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
       <label>enter url</label>
       <input type="url" v-model="url">
       <button @click="make">make short url</button>
+      <label><input type='checkbox' v-model="useHttps" checked />use https</label>
     </div>
     <div class="short-url">
       short url:
@@ -22,26 +23,55 @@ export default {
   data: function() {
     return {
       url: "",
-      shortLink: ""
+      shortLink: "",
+      useHttps: true
     };
   },
-  created: function () {
-    console.log(process.env.NODE_ENV)
+  created: function() {
+    console.log(process.env.NODE_ENV);
   },
   methods: {
     copy: function() {
       console.log("copy");
     },
     make: function() {
-      const baseHOST = process.env.NODE_ENV === "development" ? "http://localhost:8080" : "https://l.codingall.me"
+      const baseHOST =
+        process.env.NODE_ENV === "development"
+          ? "http://localhost:8080"
+          : "https://l.codingall.me";
       console.log(this.url);
       if (!this.checkURL()) {
         alert("URL INVALID");
         return false;
       }
+      this.useHttps ? this.addHttpsToFromURL() : this.addHttpToFromURL();
       const hash = shortid.generate();
       this.shortLink = `${baseHOST}/${hash}`;
-      this.$store.dispatch(MAKE_URL, { key: hash, from: this.url, to: this.shortLink});
+      this.$store.dispatch(MAKE_URL, {
+        key: hash,
+        from: this.url,
+        to: this.shortLink
+      });
+    },
+    addHttpsToFromURL: function() {
+      if (this.url.startsWith("http://")) {
+        const split = this.url.split("http://");
+        this.url = `https://${split[1]}`;
+      }
+
+      if (!this.url.startsWith("https://")) {
+        this.url = `https://${this.url}`;
+      }
+    },
+    addHttpToFromURL: function() {
+      if (this.url.startsWith("https://")) {
+        const split = this.url.split("https://");
+        this.url = `http://${split[1]}`;
+      }
+
+      if (!this.url.startsWith("http://")) {
+        this.url = `http://${this.url}`;
+      }
     },
     checkURL: function() {
       const expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
